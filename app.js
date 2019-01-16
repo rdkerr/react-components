f4f9a4d07a99a6527d1f316a9436f58095e882e4
rdkerr <rogerdkerr@gmail.com>
2019-01-16 14:20:37 -0700

    Added event listeners

--- app.js
+++ app.js
@@ -1,1 +1,23 @@
 // TODO
+var GroceryListItem = (props) => {
+  var onListItemClick = (event) => {
+    console.log('I got clicked');
+  };
+
+  return (
+    <ul>
+      <li onClick={onListItemClick}>{props.groceries[0]}</li>
+      <li onClick={onListItemClick}>{props.groceries[1]}</li>
+    </ul>
+  );
+}
+
+var App = () => (
+  <div>
+    <h2>My Grocery List</h2>
+    <GroceryListItem groceries={['kale', 'cucumber']}/>
+  </div>
+);
+
+
+ReactDOM.render(<App />, document.getElementById("app"));
